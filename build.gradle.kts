8f7e04a191a2faa3703acf1850b5d1401583e483
dumdumbich <dmdumbich@yandex.ru>
2023-03-15 12:45:39 +0300

    Staging server Hello World

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+val kotlinVersion: String by project
+val ktorVersion: String by project
+val logbackVersion: String by project
+
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.6.0"
     application
 }
 
-group = "org.example"
+group = "com.dumdumbich"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,6 +15,18 @@ repositories {
 }
 
 dependencies {
+    // Ktor
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+
+    // LogBack
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    // Kotlin
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+
+    // Test
     testImplementation(kotlin("test"))
 }
 
@@ -18,10 +34,10 @@ tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(8)
-}
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
